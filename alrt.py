17e9fef9b73b5e7777dfcf36222147a076f822c4
Gaurav Tak <gauravtak996@gmail.com>
2024-01-13 10:05:17 +0530

    user input and checking against current_time

--- alrt.py
+++ alrt.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
+import time
+import os
+
+ALARM_FILE = 'alarm_file.txt'
+# Function to set alarm
+def set_alarm():
+    
+
+    # Take user input for alarm
+    user_input = input("Set your alarm (HH:MM): ")
+
+    try:
+        time_obj = datetime.datetime.strptime(user_input, "%H:%M")
+        set_alarm_value = time_obj.time().strftime("%H:%M")
+
+        with open(ALARM_FILE, "w") as file:
+            file.write(set_alarm_value)
+
+
+        print("Your alarm: ", set_alarm_value)
+        return set_alarm_value 
+    except ValueError:
+        print("Invalid time format. Please enter time in HH:MM:SS format.")
 
 def main():
     parser = argparse.ArgumentParser(description='Description of your alrt application.')
@@ -11,6 +35,20 @@ def main():
 
     # Your application logic goes here
     print("Hello from alrt!")
+
+    alarm_time = set_alarm()
+
+    # extracting current time of your laptop
+    current_time = datetime.datetime.now().strftime("%H:%M")
+
+    # check alarm against the current time
+    if current_time == alarm_time:
+        print("This is alarm")
+    else:
+        print("Wait for alarm")
+    
+    
+        
 
 if __name__ == "__main__":
     main()
