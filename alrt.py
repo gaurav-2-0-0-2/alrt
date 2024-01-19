d2feb64ef30faf0a24285115afab9f50f7eb93e7
Gaurav Tak <gauravtak996@gmail.com>
2024-01-19 22:07:56 +0530

    add notification

--- alrt.py
+++ alrt.py
@@ -4,6 +4,8 @@ import argparse
 import datetime
 import time
 import os
+import subprocess
+import schedule
 
 ALARM_FILE = 'alarm_file.txt'
 # Function to set alarm
@@ -26,29 +28,73 @@ def set_alarm():
     except ValueError:
         print("Invalid time format. Please enter time in HH:MM:SS format.")
 
+# Function that reads alarm_file.txt for stored alarm
+def read_alarm():
+
+    # reading the alarm from 'alarm_file.txt'
+    try:
+        with open(ALARM_FILE, "r") as file:
+            alarm_read = file.read().strip()
+        return alarm_read
+    except FileNotFoundError:
+       return None
+       
+def notify():
+        subprocess.run(["/usr/bin/notify-send", "--icon=error", "This is your alarm"])
+
+
 def main():
     parser = argparse.ArgumentParser(description='Description of your alrt application.')
     # Add your custom command-line arguments here, if any
     # parser.add_argument('--example', help='Example argument')
 
+
+
     args = parser.parse_args()
 
-    # Your application logic goes here
     print("Hello from alrt!")
 
-    alarm_time = set_alarm()
+    set_alarm() 
+    alarm_time = read_alarm()
 
-    # extracting current time of your laptop
-    current_time = datetime.datetime.now().strftime("%H:%M")
 
-    # check alarm against the current time
-    if current_time == alarm_time:
-        print("This is alarm")
-    else:
-        print("Wait for alarm")
-    
-    
-        
+
+
+
+
+######### more efficient approach #############
+######### this approach uses scheduling #############
+
+    schedule.every().day.at(alarm_time).do(notify)
+
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+######### more efficient approach #############
+
+
+
+######### not so efficient approach #############
+######### this approach uses a loop until the condition reaches which wastes resources #############
+    #while True:
+    #     # extracting current time of your laptop
+    #     current_time = datetime.datetime.now().strftime("%H:%M")
+    #     stored_alarm = read_alarm() 
+
+    #     # check alarm against the current time
+    #     if current_time == stored_alarm:
+    #         subprocess.run(["/usr/bin/notify-send", "--icon=error", "This is your error message ..."])
+    #         #print("This is alarm")
+    #         break
+    #     else:
+    #         print("wait for the alarm")
+    #         break
+            
+         
+         
+             
 
 if __name__ == "__main__":
     main()
